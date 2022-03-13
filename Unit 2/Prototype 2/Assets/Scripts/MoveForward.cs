0a1a087011fef3385ad8326fd7e27eada3ab5222
cedric <cheijlman@roc-dev.com>
2022-03-13 16:37:03 +0100

    unit 1, unit 2, unit 3.1

---
+++ Unit 2/Prototype 2/Assets/Scripts/MoveForward.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveForward : MonoBehaviour
+{
+
+    public float pizzaSpeed = 40.0f;
+   
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector3.forward * Time.deltaTime * pizzaSpeed);
+    }
+}
---
+++ Unit 3/Prototype 3/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float jumpForce = 10;
+    public float gravityModifier;
+    private Rigidbody playerRb;
+    public bool isOnGround = true;
+    void Start()
+    {
+        playerRb = GetComponent<Rigidbody>();
+        Physics.gravity *= gravityModifier;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+     
+        if (Input.GetKeyDown(KeyCode.Space) && isOnGround)
+        {
+            playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            isOnGround = false;
+        }
+
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        isOnGround = true;
+    }
+
+}
